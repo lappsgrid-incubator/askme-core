4d3d0cb6f02e108f09842aeb90c0513b54dd9240
Kevin Ros <kevinros1190@gmail.com>
2019-08-06 11:38:20 -0400

    added utils

--- src/main/groovy/org/lappsgrid/askme/core/Utils.groovy
+++ src/main/groovy/org/lappsgrid/askme/core/Utils.groovy
@@ -1,4 +1,34 @@
 package org.lappsgrid.askme.core
 
+/**
+ *
+ */
 class Utils {
+    /**
+     * Simple duration formating.
+     * <p>
+     * Using the Duration and Period classes from java.time or Joda time
+     * to format the duration is just as much work and no better than simply applying
+     * div and mod to the input value (at least for our use case).
+     *
+     * @param input the duration to format
+     * @return A string in the format DDd HH:MM:SS.sss
+     */
+    static String format(long input) {
+        long msec = input % 1000
+        long seconds = input / 1000
+        long minutes = seconds / 60
+        seconds = seconds % 60
+        long hours = minutes / 60
+        minutes = minutes % 60
+        // Only display the days if we need to.
+        if (hours >= 24) {
+            long days = hours / 24
+            hours = hours % 24
+            return String.format("%dd %02d:%02d:%02d.%03d", days, hours, minutes, seconds, msec);
+        }
+        return String.format("%d:%02d:%02d.%03d", hours, minutes, seconds, msec);
+    }
+
+
 }
