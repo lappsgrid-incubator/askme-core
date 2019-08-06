97b1999ef6da616b774b928a28bb466af2fbf067
Kevin Ros <kevinros1190@gmail.com>
2019-08-06 11:49:20 -0400

    added model package

--- src/main/groovy/org/lappsgrid/askme/core/model/Sentence.groovy
+++ src/main/groovy/org/lappsgrid/askme/core/model/Sentence.groovy
@@ -1,4 +1,35 @@
 package org.lappsgrid.askme.core.model
 
-class Sentence {
+import java.util.function.Consumer
+
+/**
+ *
+ */
+class Sentence implements Iterable<Token> {
+    int start
+    int end
+
+    String text
+    List<Token> tokens
+
+    Sentence() {
+        tokens = []
+    }
+
+    Sentence(String text, List<Token> tokens) {
+        this.text = text
+        this.tokens = tokens
+    }
+
+    Iterator<Token> tokens() { return tokens.iterator() }
+
+    @Override
+    Iterator<Token> iterator() {
+        return tokens.iterator()
+    }
+
+    @Override
+    void forEach(Consumer<? super Token> action) {
+        tokens.forEach(action)
+    }
 }
--- src/main/groovy/org/lappsgrid/askme/core/model/Token.groovy
+++ src/main/groovy/org/lappsgrid/askme/core/model/Token.groovy
@@ -1,4 +1,14 @@
 package org.lappsgrid.askme.core.model
 
+/**
+ *
+ */
 class Token {
+    int start
+    int end
+
+    String word
+    String lemma
+    String pos
+    String category
 }
