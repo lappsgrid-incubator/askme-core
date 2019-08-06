28f8ff9e5f680600ac861d5f752cda7dac61ea14
Kevin Ros <kevinros1190@gmail.com>
2019-08-06 11:51:27 -0400

    added api

--- src/main/groovy/org/lappsgrid/askme/core/api/QueryProcessor.groovy
+++ src/main/groovy/org/lappsgrid/askme/core/api/QueryProcessor.groovy
@@ -1,4 +1,16 @@
 package org.lappsgrid.askme.core.api
 
-class QueryProcessor {
+/**
+ * Interface for classes that transform the free-form natural language text entered by
+ * the user into a query suitable for the search engine (Solr, ElasticSearch)
+ */
+public interface QueryProcessor {
+    /**
+     * Transforms the question from the user into a valid query string.
+     *
+     * @param question a free form question from the user.
+     * @return A query string suitable to be sent to the underlying
+     *         search engine (Solr, ElasticSearch, etc).
+     */
+    public Query transform(final String question);
 }
