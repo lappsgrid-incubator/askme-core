0fe5d1bc6c460310ac3de084db813ad65ec4012e
Keith Suderman <suderman@cs.vassar.edu>
2019-09-12 15:32:29 -0500

    Code clean up

--- src/main/groovy/org/lappsgrid/askme/core/model/Document.groovy
+++ src/main/groovy/org/lappsgrid/askme/core/model/Document.groovy
@@ -1,7 +1,6 @@
 package org.lappsgrid.askme.core.model
 
 import org.lappsgrid.askme.core.api.Scores
-import org.lappsgrid.askme.core.model.Section
 
 /**
  *
@@ -12,54 +11,23 @@ class Document {
     String pmc
     String doi
     String year
-//    String title
-//    String articleAbstract
-//    String intro
-//    String discussion
-//    String results
 
     Section title
     Section articleAbstract
-//    Section body
-    String body
+    Section body
 
     String path
 
     /** The total score for the document. */
     float score
+
     /** The scores for each section. */
     Map<String, Scores> scores
-
-    /*
-                d.pmid = doc.getFieldValue('pmid')
-            d.pmc = doc.getFieldValue('pmc')
-            d.doi = doc.getFieldValue('doi')
-            d.year = doc.getFieldValue('year')
-            d.title = doc.getFieldValue('title')
-            d.path = doc.getFieldValue('path')
-
-     */
 
     Document() {
         score = 0.0f
         scores = [:]
     }
-
-//    Document(SolrDocument document) {
-//        ['id', 'pmid','pmc','doi','year','path'].each { field ->
-//            this.setProperty(field, document.getFieldValue(field))
-//        }
-//        String temp = document.getFieldValue(Fields.TITLE)
-//        if (temp) {
-//            title = temp.toLowerCase()
-//        }
-//
-//        intro = document.getFieldValue(Fields.INTRO)
-//        articleAbstract = document.getFieldValue(Fields.ABSTRACT)
-//        discussion = document.getFieldValue(Fields.DISCUSSION)
-//        score = 0.0f
-//        scores = [:]
-//    }
 
     void addScore(String section, String algorithm, float value) {
         Scores forSection = scores.get(section)
